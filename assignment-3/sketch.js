4bf2b73beeebd6c752a07fd0dc5c0bd237963eec
brimocars <bsm1875@rit.edu>
2025-01-31 07:52:27 -0500

    b

--- assignment-3/sketch.js
+++ assignment-3/sketch.js
@@ -4,81 +4,91 @@ const fieldSpacing = 15;
 const lineLength = 10;
 const fields = [];
 const diameter = 30
+const maxSpeed = 5;
+const eraserMax = 300;
+let eraserTimer = 0;
 
 const circles = [
   {
     x: getRandomInts(0, width),
     y: getRandomInts(0, height),
     color: [255,0,0],
-    speedMultiplier: Math.random() * 5,
+    speedMultiplier: Math.random() * maxSpeed,
   },
   {
     x: getRandomInts(0, width),
     y: getRandomInts(0, height),
     color: [255,128,0],
-    speedMultiplier: Math.random() * 5,
+    speedMultiplier: Math.random() * maxSpeed,
   },
   {
     x: getRandomInts(0, width),
     y: getRandomInts(0, height),
     color: [255,255,0],
-    speedMultiplier: Math.random() * 5,
+    speedMultiplier: Math.random() * maxSpeed,
   },
   {
     x: getRandomInts(0, width),
     y: getRandomInts(0, height),
     color: [128,255,0],
-    speedMultiplier: Math.random() * 5,
+    speedMultiplier: Math.random() * maxSpeed,
   },
   {
     x: getRandomInts(0, width),
     y: getRandomInts(0, height),
     color: [0,255,0],
-    speedMultiplier: Math.random() * 5,
+    speedMultiplier: Math.random() * maxSpeed,
   },
   {
     x: getRandomInts(0, width),
     y: getRandomInts(0, height),
     color: [0,255,128],
-    speedMultiplier: Math.random() * 5,
+    speedMultiplier: Math.random() * maxSpeed,
   },
   {
     x: getRandomInts(0, width),
     y: getRandomInts(0, height),
     color: [0,255,255],
-    speedMultiplier: Math.random() * 5,
+    speedMultiplier: Math.random() * maxSpeed,
   },
   {
     x: getRandomInts(0, width),
     y: getRandomInts(0, height),
     color: [0,128,255],
-    speedMultiplier: Math.random() * 5,
+    speedMultiplier: Math.random() * maxSpeed,
   },
   {
     x: getRandomInts(0, width),
     y: getRandomInts(0, height),
     color: [0,0,255],
-    speedMultiplier: Math.random() * 5,
+    speedMultiplier: Math.random() * maxSpeed,
   },
   {
     x: getRandomInts(0, width),
     y: getRandomInts(0, height),
     color: [128,0,255],
-    speedMultiplier: Math.random() * 5,
+    speedMultiplier: Math.random() * maxSpeed,
   },
   {
     x: getRandomInts(0, width),
     y: getRandomInts(0, height),
     color: [255,0,255],
-    speedMultiplier: Math.random() * 5,
+    speedMultiplier: Math.random() * maxSpeed,
   },
   {
     x: getRandomInts(0, width),
     y: getRandomInts(0, height),
     color: [255,0,128],
-    speedMultiplier: Math.random() * 5,
+    speedMultiplier: Math.random() * maxSpeed,
   },
 ]
+
+const eraser = {
+  x: 0,
+  y: getRandomInts(0, height),
+  color: [0,0,0],
+  speedMultiplier: Math.random() * maxSpeed,
+}
 
 const noiseSpeedDivisor = 5;
 const noiseScale = 0.05;
@@ -99,11 +109,13 @@ function setup() {
 
 function draw() {
   circles.forEach((circ) => {
-    fill(circ.color[0], circ.color[1], circ.color[2], 3);
+    fill(circ.color[0], circ.color[1], circ.color[2], 5 * circ.speedMultiplier);
 
     
-    const teleport = Math.random() > 0.999;
-    if (teleport) {
+    const teleportNumber = Math.random();
+    const smallTeleport = teleportNumber > 0.990
+    const bigTeleport = teleportNumber > 0.999
+    if (bigTeleport) {
       circ.x = getRandomInts(0, height)
       circ.y = getRandomInts(0, height)
       circ.level += 1;
@@ -111,14 +123,31 @@ function draw() {
         addNewField();
       }
     } else {
+      let addedX = 0;
+      let addedY = 0;
+      if (smallTeleport) {
+        addedX = getRandomInts(-10, 11)
+        addedY = getRandomInts(-10, 11)
+      }
       const angle = fields[circ.level][Math.floor(circ.x)][Math.floor(circ.y)].angle
-      circ.x += cos(angle);
-      circ.y += sin(angle);
+      circ.x += cos(angle) * circ.speedMultiplier + addedX;
+      circ.y += sin(angle) * circ.speedMultiplier + addedY;
       circ.x = (circ.x + fields[circ.level].length) % fields[circ.level].length
       circ.y = (circ.y + fields[circ.level][0].length) % fields[circ.level][0].length  
     }
     circle(circ.x, circ.y, diameter);
   })
+
+  // if (eraserTimer !== 0) {
+  //   fill(0,0,0);
+  //   eraserTimer += 1;
+  //   circle(eraser.x, eraser.y, diameter * 3);
+  //   if (eraser > eraserMax) {
+  //     eraserTimer = 0;
+  //   }
+  // } else {
+  //   const randoms = 
+  // }
 }
 
 /**
