f48b9410fc186d55e26fa16e714481dba2005727
Brian Mocarski <bsm1875@rit.edu>
2025-02-28 09:46:03 -0500

    added an ant to my life

--- life/sketch.js
+++ life/sketch.js
@@ -1,33 +1,57 @@
-const width = 800;
-const height = 800;
 const cellSize = 10;
+const width = window.innerWidth - 200 - ((window.innerWidth - 200) % cellSize);
+const height = window.innerHeight - ((window.innerHeight) % cellSize);
 let cells = [];
 let frameRateSlider;
+let lastTouchedByAntToSetSlider;
+let lastTouchedByAntToSet;
+
+const ant = {
+  x: getRandomInts(0, width / cellSize),
+  y: getRandomInts(0, height / cellSize),
+  direction: getRandomInts(0, 4, 1)
+}
+const cellDirection = 'LR';
+const letterMappings = {
+  'L': -1,
+  'R': 1,
+  'U': 2,
+  'N': 0,
+}
+let translatedTurns = [];
 
 function setup() {
-  createCanvas(width + 100, height);
+  createCanvas(width + 200, height);
   noStroke();
-  fill(100,100,100)
-  rect(width, 0, 100, height)
+  fill(200, 200, 200)
+  rect(width, 0, 200, height)
 
-  for(let x = 0; x < width; x += cellSize) {
+  for (let x = 0; x < width; x += cellSize) {
     const row = [];
-    for(let y = 0; y < width; y += cellSize) {
-      row.push(new Cell(x, y, Math.random() > 0.9))
+    for (let y = 0; y < height; y += cellSize) {
+      row.push(new Cell(x, y, Math.random() > 0.95))
     }
     cells.push(row);
   }
+
+  translatedTurns.length = 0;
+  for (let i = 0; i < cellDirection.length; i++) {
+    translatedTurns.push(letterMappings[cellDirection[i]]);
+  }
+
   frameRate(10);
   setUpControls();
 }
 
 function draw() {
+  lastTouchedByAntToSet = lastTouchedByAntToSetSlider.value();
   cells.forEach((row) => {
     row.forEach((c) => {
       c.drawCell();
     })
   })
   updateCells();
+  moveAnt();
 }
 
 function updateCells() {
@@ -37,40 +61,44 @@ function updateCells() {
     const newRow = [];
     for (let j = 0; j < cells[0].length; j++) {
       const currentCell = cells[i][j];
-      const newCell = new Cell(currentCell.x, currentCell.y, currentCell.isAlive);
-      let neighborsAlive = 0;
-      if (cells[((i-1) + cells.length) % cells.length]?.[((j) + cells[0].length) % cells[0].length]?.isAlive) {
-        neighborsAlive++;
-      }
-      if (cells[((i+1) + cells.length) % cells.length]?.[((j) + cells[0].length) % cells[0].length]?.isAlive) {
-        neighborsAlive++;
-      }
-      if (cells[((i-1) + cells.length) % cells.length]?.[((j+1) + cells[0].length) % cells[0].length]?.isAlive) {
-        neighborsAlive++;
-      }
-      if (cells[((i) + cells.length) % cells.length]?.[((j+1) + cells[0].length) % cells[0].length]?.isAlive) {
-        neighborsAlive++;
-      }
-      if (cells[((i+1) + cells.length) % cells.length]?.[((j+1) + cells[0].length) % cells[0].length]?.isAlive) {
-        neighborsAlive++;
-      }
-      if (cells[((i-1) + cells.length) % cells.length]?.[((j-1) + cells[0].length) % cells[0].length]?.isAlive) {
-        neighborsAlive++;
-      }
-      if (cells[((i) + cells.length) % cells.length]?.[((j-1) + cells[0].length) % cells[0].length]?.isAlive) {
-        neighborsAlive++;
-      }
-      if (cells[((i+1) + cells.length) % cells.length]?.[((j-1) + cells[0].length) % cells[0].length]?.isAlive) {
-        neighborsAlive++;
-      }
-
-      if (currentCell.isAlive) {
-        if (neighborsAlive < 2 || neighborsAlive > 3) {
-          newCell.isAlive = false;
-        }
+      const newCell = new Cell(currentCell.x, currentCell.y, currentCell.isAlive, currentCell.lastTouchedByAnt);
+      if (newCell.lastTouchedByAnt > 0) {
+        newCell.lastTouchedByAnt--;
       } else {
-        if (neighborsAlive === 3) {
-          newCell.isAlive = true;
+        let neighborsAlive = 0;
+        if (cells[((i - 1) + cells.length) % cells.length]?.[((j) + cells[0].length) % cells[0].length]?.isAlive) {
+          neighborsAlive++;
+        }
+        if (cells[((i + 1) + cells.length) % cells.length]?.[((j) + cells[0].length) % cells[0].length]?.isAlive) {
+          neighborsAlive++;
+        }
+        if (cells[((i - 1) + cells.length) % cells.length]?.[((j + 1) + cells[0].length) % cells[0].length]?.isAlive) {
+          neighborsAlive++;
+        }
+        if (cells[((i) + cells.length) % cells.length]?.[((j + 1) + cells[0].length) % cells[0].length]?.isAlive) {
+          neighborsAlive++;
+        }
+        if (cells[((i + 1) + cells.length) % cells.length]?.[((j + 1) + cells[0].length) % cells[0].length]?.isAlive) {
+          neighborsAlive++;
+        }
+        if (cells[((i - 1) + cells.length) % cells.length]?.[((j - 1) + cells[0].length) % cells[0].length]?.isAlive) {
+          neighborsAlive++;
+        }
+        if (cells[((i) + cells.length) % cells.length]?.[((j - 1) + cells[0].length) % cells[0].length]?.isAlive) {
+          neighborsAlive++;
+        }
+        if (cells[((i + 1) + cells.length) % cells.length]?.[((j - 1) + cells[0].length) % cells[0].length]?.isAlive) {
+          neighborsAlive++;
+        }
+
+        if (currentCell.isAlive) {
+          if (neighborsAlive < 2 || neighborsAlive > 3) {
+            newCell.isAlive = false;
+          }
+        } else {
+          if (neighborsAlive === 3) {
+            newCell.isAlive = true;
+          }
         }
       }
       newRow.push(newCell);
@@ -81,43 +109,80 @@ function updateCells() {
 }
 
 class Cell {
-  constructor(x, y, isAlive) {
+  constructor(x, y, isAlive, lastTouchedByAnt) {
     this.x = x;
     this.y = y;
     this.isAlive = isAlive
+    this.lastTouchedByAnt = lastTouchedByAnt ?? 0;
   }
 
   drawCell() {
-    if (this.isAlive) {
-      fill(0,0,0);
+    if (this.lastTouchedByAnt > 1 && this.isAlive) {
+      fill(162, 162, 162);
+    } else if (this.lastTouchedByAnt > 1 && !this.isAlive) {
+      fill(94, 94, 94);
+    } else if (this.isAlive) {
+      fill(0, 0, 0);
     } else {
       fill(255, 255, 255);
     }
-    rect(this.x, this.y, cellSize, cellSize)
+    square(this.x, this.y, cellSize)
   }
 }
+
+function moveAnt() {
+  const currentCell = cells[ant.x][ant.y];
+  ant.direction = (ant.direction + translatedTurns[currentCell.isAlive ? 1 : 0] + 4) % 4;
+  currentCell.isAlive = !currentCell.isAlive;
+  currentCell.lastTouchedByAnt = lastTouchedByAntToSet;
+  switch (ant.direction) {
+    case 0: ant.y -= 1; break;
+    case 1: ant.x += 1; break;
+    case 2: ant.y += 1; break;
+    case 3: ant.x -= 1; break;
+  }
+
+  const cellsPerRow = Math.floor(width / cellSize);
+  const cellsPerColumn = Math.floor(height / cellSize);
+  ant.x = (ant.x + cellsPerRow) % cellsPerRow;
+  ant.y = (ant.y + cellsPerColumn) % cellsPerColumn;
+  console.log(ant);
+
+  fill(255, 128, 0);
+  square(ant.x * cellSize, ant.y * cellSize, cellSize)
+
+
+  fill(currentCell.isAlive ? color(192, 192, 192) : color(64, 64, 64));
+  square(currentCell.x * cellSize, currentCell.y * cellSize, cellSize);
+}
+
 
 function getRandomInts(min, max, howManyInts = 1) {
   if (howManyInts === 1) {
     return Math.floor(Math.random() * (max - min) + min);
   }
-  
+
   const numbers = [];
   for (let i = 0; i < howManyInts; i++) {
-      numbers.push(Math.floor(Math.random() * (max - min) + min));
+    numbers.push(Math.floor(Math.random() * (max - min) + min));
   }
   return numbers;
 }
 
 function setUpControls() {
   removeElements();
-  
-  textSize(10)
-  fill(0,0,0)
-  
+
+  textSize(14)
+  fill(0, 0, 0)
+
   text('Framerate', width + 10, 75)
   frameRateSlider = createSlider(0, 60, 15, 1);
   frameRateSlider.position(width + 10, 75);
-  frameRateSlider.size(80);
+  frameRateSlider.size(160);
   frameRateSlider.mouseClicked(() => frameRate(frameRateSlider.value()))
+  
+  text('Ant protection duration', width + 10, 125)
+  lastTouchedByAntToSetSlider = createSlider(0, 200, 50, 1);
+  lastTouchedByAntToSetSlider.position(width + 10, 125);
+  lastTouchedByAntToSetSlider.size(160);
 }
