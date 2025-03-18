40e22ad5a0458c868c6ac03c36aa654f15d7bf36
Brian <bsm1875@rit.edu>
2025-03-18 12:13:22 -0400

    made improvements, broke neighbors

--- project-2/sketch.js
+++ project-2/sketch.js
@@ -2,15 +2,16 @@
  * TODO:
  * - add more states, something with prevStates and branching paths maybe
  * - make mobile friendly?
- * - make the code better for the mouse events
- * - cell size slider
+ * - FIX NEIGHBORS
  */
 
+const defaultCellSize = 25;
 
-const cellSize = 25;
-const width = window.innerWidth - 200 - ((window.innerWidth - 200) % cellSize);
-const height = window.innerHeight - ((window.innerHeight) % cellSize);
-const sectionWidth = width / 2;
+let width = window.innerWidth - 200 - ((window.innerWidth - 200) % defaultCellSize);
+let height = window.innerHeight - ((window.innerHeight) % defaultCellSize);
+const originalWidth = width;
+const originalHeight = height;
+let sectionWidth = width / 2;
 let cells = [];
 let frameRateSlider;
 let currentMouseDrag;
@@ -40,11 +41,12 @@ const rules = {
     const newEndX = (endX % sectionWidth) + sectionWidth
     const newEndY = ((endY + height) % height);
 
+    // floating point error correction
     if (Math.abs(newEndX - endX) < 0.000001 && Math.abs(newEndY - endY) < 0.000001) {
       stroke(state.h, 50, 50);
       line(state.x, state.y, endX, endY);
     }
-    if (newEndX + state.diameter / 2 <= width) {
+    if (newEndX + state.diameter / 2 <= width && newEndX - state.diameter / 2 >= sectionWidth) {
       circle(newEndX, newEndY, state.diameter);
     }
     state.x = newEndX;
@@ -84,21 +86,31 @@ let input1;
 let inputPlus;
 let inputMinus;
 let aliveChanceSlider;
+let cellSizeSlider;
+let cellSize;
 
 function setup() {
-  createCanvas(width + 200, height);
-  background(0);
-  fill(255, 255, 255);
-  stroke(0, 0, 0)
-  rect(0, 0, sectionWidth, height);
-  rect(sectionWidth, 0, sectionWidth, height);
-  rect(width, 0, 200, height);
+  doSetupStuff(true);
+}
 
-  setUpControls();
-
-  angleMode(DEGREES);
-  colorMode('hsl');
-  noStroke();
+function doSetupStuff(firstTime = false) {
+  if (firstTime) {
+    createCanvas(width + 200, height);
+    background(0);
+    fill(255, 255, 255);
+    stroke(0, 0, 0)
+    rect(0, 0, sectionWidth, height);
+    rect(sectionWidth, 0, sectionWidth, height);
+    rect(width, 0, 200, height);
+    setUpControls();
+    angleMode(DEGREES);
+    colorMode('hsl');
+    noStroke();
+  } else {
+    cells.length = 0;
+    axiom = '';
+    draw();
+  }
 
   axiom = '';
   for (let i = 0; i < generations; i++) {
@@ -120,7 +132,6 @@ function draw() {
   rect(sectionWidth, 0, sectionWidth, height);
   pop()
 
-  generations = generationsSlider.value();
   defaultState.lineLength = lineLengthSlider.value();
   defaultState.diameter = diameterSlider.value();
   transformations['0'] = input0.value();
@@ -138,6 +149,7 @@ function draw() {
 }
 
 function regenerateAxiom() {
+  generations = generationsSlider.value();
   axiom = '';
   cells.forEach((row) => {
     let alivitivity = 0
@@ -158,6 +170,11 @@ function regenerateAxiom() {
 
   state = structuredClone(defaultState);
   push();
+  fill(100, 100, 100);
+  rect(sectionWidth, 0, sectionWidth, height);
+  pop();
+
+  push();
   fill(0, 0, 0);
   stroke(0, 0, 0);
   strokeWeight(5);
@@ -174,44 +191,17 @@ function updateCells() {
     const newRow = [];
     for (let j = 0; j < cells[0].length; j++) {
       const currentCell = cells[i][j];
-      const newCell = new Cell(currentCell.x, currentCell.y, currentCell.isAlive);
-
-      let neighborsAlive = 0;
-      if (cells[((i - 1) + cells.length) % cells.length]?.[((j) + cells[0].length) % cells[0].length]?.isAlive) {
-        neighborsAlive++;
-      }
-      if (cells[((i + 1) + cells.length) % cells.length]?.[((j) + cells[0].length) % cells[0].length]?.isAlive) {
-        neighborsAlive++;
-      }
-      if (cells[((i - 1) + cells.length) % cells.length]?.[((j + 1) + cells[0].length) % cells[0].length]?.isAlive) {
-        neighborsAlive++;
-      }
-      if (cells[((i) + cells.length) % cells.length]?.[((j + 1) + cells[0].length) % cells[0].length]?.isAlive) {
-        neighborsAlive++;
-      }
-      if (cells[((i + 1) + cells.length) % cells.length]?.[((j + 1) + cells[0].length) % cells[0].length]?.isAlive) {
-        neighborsAlive++;
-      }
-      if (cells[((i - 1) + cells.length) % cells.length]?.[((j - 1) + cells[0].length) % cells[0].length]?.isAlive) {
-        neighborsAlive++;
-      }
-      if (cells[((i) + cells.length) % cells.length]?.[((j - 1) + cells[0].length) % cells[0].length]?.isAlive) {
-        neighborsAlive++;
-      }
-      if (cells[((i + 1) + cells.length) % cells.length]?.[((j - 1) + cells[0].length) % cells[0].length]?.isAlive) {
-        neighborsAlive++;
-      }
-
+      const neighborsAlive = currentCell.calculateNeighborsAlive();
       if (currentCell.isAlive) {
         if (neighborsAlive < 2 || neighborsAlive > 3) {
-          newCell.isAlive = false;
+          currentCell.isAlive = false;
         }
       } else {
         if (neighborsAlive === 3) {
-          newCell.isAlive = true;
+          currentCell.isAlive = true;
         }
       }
-      newRow.push(newCell);
+      newRow.push(currentCell);
     }
     newCells.push(newRow);
   }
@@ -222,7 +212,8 @@ class Cell {
   constructor(x, y, isAlive) {
     this.x = x;
     this.y = y;
-    this.isAlive = isAlive
+    this.isAlive = isAlive;
+    this.neighbors = [];
   }
 
   drawCell() {
@@ -233,9 +224,22 @@ class Cell {
     }
     square(this.x, this.y, cellSize)
   }
+
+  calculateNeighborsAlive() {
+    return this.neighbors.filter((neighbor) => neighbor.isAlive).length;
+  }
 }
 
 function regenerateCells() {
+  push();
+  fill(255, 255, 255);
+  rect(0, 0, originalWidth, originalHeight);
+  pop();
+  cellSize = cellSizeSlider.value();
+  width = window.innerWidth - 200 - ((window.innerWidth - 200) % cellSize);
+  height = window.innerHeight - ((window.innerHeight) % cellSize);
+  sectionWidth = width / 2;
+
   cells.length = 0;
   for (let x = 0; x < sectionWidth; x += cellSize) {
     const row = [];
@@ -244,6 +248,25 @@ function regenerateCells() {
     }
     cells.push(row);
   }
+
+  for (let x = 0; x < cells.length; x++) {
+    for (let y = 0; y < cells[x].length; y++) {
+      const neighbors = [];
+      neighbors.push(cells[weirdModThing(x+1, cells.length)][weirdModThing(y-1, cells[x].length)])
+      neighbors.push(cells[weirdModThing(x+1, cells.length)][weirdModThing(y, cells[x].length)])
+      neighbors.push(cells[weirdModThing(x+1, cells.length)][weirdModThing(y+1, cells[x].length)])
+      neighbors.push(cells[weirdModThing(x, cells.length)][weirdModThing(y+1, cells[x].length)])
+      neighbors.push(cells[weirdModThing(x, cells.length)][weirdModThing(y-1, cells[x].length)])
+      neighbors.push(cells[weirdModThing(x-1, cells.length)][weirdModThing(y+1, cells[x].length)])
+      neighbors.push(cells[weirdModThing(x-1, cells.length)][weirdModThing(y, cells[x].length)])
+      neighbors.push(cells[weirdModThing(x-1, cells.length)][weirdModThing(y-1, cells[x].length)])
+      cells[x][y].neighbors = neighbors;
+    }
+  }
+}
+
+function weirdModThing(numberToMod, total) {
+  return (numberToMod + total) % total;
 }
 
 /**
@@ -263,7 +286,7 @@ function getRandomInts(min, max, howManyInts = 1) {
   return numbers;
 }
 
-function mousePressed() {
+function mouseThing() {
   const rowClicked = cells[(mouseX - (mouseX % cellSize)) / cellSize];
   if (!rowClicked) {
     return;
@@ -277,18 +300,12 @@ function mousePressed() {
   };
 }
 
+function mousePressed() {
+  mouseThing()
+}
+
 function mouseDragged() {
-  const rowClicked = cells[(mouseX - (mouseX % cellSize)) / cellSize];
-  if (!rowClicked) {
-    return;
-  }
-  const cellAtMouse = rowClicked[(mouseY - (mouseY % cellSize)) / cellSize]
-  if (cellAtMouse && cellAtMouse !== currentMouseDrag) {
-    cellAtMouse.isAlive = !cellAtMouse.isAlive
-    cellAtMouse.drawCell();
-    regenerateAxiom();
-    currentMouseDrag = cellAtMouse;
-  };
+  mouseThing()
 }
 
 function mouseReleased() {
@@ -301,61 +318,68 @@ function setUpControls() {
   fill(0, 0, 0);
   noStroke();
 
-  text('Framerate', width + 10, 75);
+  text('Framerate', width + 10, 25);
   frameRateSlider = createSlider(0, 20, 1, 1);
-  frameRateSlider.position(width + 10, 75);
+  frameRateSlider.position(width + 10, 25);
   frameRateSlider.size(160);
   frameRateSlider.mouseClicked(() => frameRate(frameRateSlider.value()))
   frameRate(frameRateSlider.value())
 
-  text('Generations', width + 10, 115);
+  text('Generations', width + 10, 65);
   generationsSlider = createSlider(0, 5, 1, 1);
-  generationsSlider.position(width + 10, 115);
+  generationsSlider.position(width + 10, 65);
   generationsSlider.size(160);
+  generationsSlider.mouseReleased(regenerateAxiom)
 
-  text('Line Length', width + 10, 155);
+  text('Line Length', width + 10, 105);
   lineLengthSlider = createSlider(1, 50, 10, 1);
-  lineLengthSlider.position(width + 10, 155);
+  lineLengthSlider.position(width + 10, 105);
   lineLengthSlider.size(160);  
 
-  text('Node Diameter', width + 10, 195);
+  text('Node Diameter', width + 10, 145);
   diameterSlider = createSlider(1, 50, 20, 1);
-  diameterSlider.position(width + 10, 195);
+  diameterSlider.position(width + 10, 145);
   diameterSlider.size(160);  
 
-  text('These text inputs define the rules for the l-system:', width + 10, 245, 160, 275);
+  text('These text inputs define the rules for the l-system:', width + 10, 195, 160, 100);
 
-  text('0 (draw line with circle)', width + 10, 320);
+  text('0 (draw line with circle)', width + 10, 270);
   input0 = createInput('10-');
-  input0.position(width + 10, 325);
+  input0.position(width + 10, 275);
   input0.size(160);
 
-  text('1 (draw line)', width + 10, 370);
+  text('1 (draw line)', width + 10, 320);
   input1 = createInput('11+1');
-  input1.position(width + 10, 375);
+  input1.position(width + 10, 325);
   input1.size(160);
 
-  text('+ (increase angle)', width + 10, 420);
+  text('+ (increase angle)', width + 10, 370);
   inputPlus = createInput('+');
-  inputPlus.position(width + 10, 425);
+  inputPlus.position(width + 10, 375);
   inputPlus.size(160);
 
-  text('- (decrease angle)', width + 10, 470);
+  text('- (decrease angle)', width + 10, 420);
   inputMinus = createInput('-');
-  inputMinus.position(width + 10, 475);
+  inputMinus.position(width + 10, 425);
   inputMinus.size(160);
 
-  text('When regenerating, chance for new cells to be alive', width + 10, 600, 160, 60);
+  text('Cell alive chance (when regenerating)', width + 10, 550, 160, 60);
   aliveChanceSlider = createSlider(0, 100, 50, 1);
-  aliveChanceSlider.position(width + 10, 650);
+  aliveChanceSlider.position(width + 10, 580);
   aliveChanceSlider.size(160);
+
+  text('Cell size (when regenerating)', width + 10, 600, 160, 60);
+  cellSizeSlider = createSlider(10, 100, defaultCellSize, 1);
+  cellSizeSlider.position(width + 10, 630);
+  cellSizeSlider.size(160);
+  cellSize = cellSizeSlider.value();
 
   const button = createButton('Regenerate Game of Life');
   button.size(180);
   button.mouseClicked(() => {
-    regenerateCells(); 
+    doSetupStuff(); 
     draw();
   });
-  button.position(width + 10, 700);
+  button.position(width + 10, 690);
   pop();
 }
